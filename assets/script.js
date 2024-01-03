efb5771c45e8e6a594583e3c02e894585446e889
xwhoknowz <jeremiah2498@gmail.com>
2024-01-02 20:41:45 -0600

    Created an array of objects containing the 5 quiz questions

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+var quiz = [
+    questionOne = {
+        question: "In Dragon Ball, which of the Dragon Balls did Grandpa Gohan leave to Goku as a momento?",
+        choices: ["Two Star Ball", "Four Star Ball", "Five Star Ball", "Seven Star Ball"],
+        correctAnswer: "Four Star Ball"
+
+    },
+
+    questionTwo = {
+        question: "In Dragon Ball, what iconic move does Goku learn after bringing Master Roshi to Fire Mountain?",
+        choices: ["Afterimage Technique", "Solar Flare", "Kamehameha Wave", "Thunder Shock Surprise"],
+        correctAnswer: "Kamehameha Wave"
+    },
+
+    questionThree = {
+        question: "In Dragon Ball, who does Son Goku train alongside under the tutelage of Master Roshi?",
+        choices: ["Yamcha", "Krillin", "Tien Shienhan", "Chiaotzu"],
+        correctAnswer: "Krillin"
+    },
+
+    questionFour = {
+        question: "In Dragon Ball, which member of the notorious Red Ribbon Army does Goku befriend?",
+        choices: ["Geneneral Blue", "Colonel Silver", "General White", "Eighter the Android"],
+        correctAnswer: "Eighter the Android"
+    },
+
+    questionFive = {
+        question: "In Dragon Ball, what move is used by Master Mutaito to defeat Demon King Piccolo",
+        choices: ["Kamehameha Wave", "Dodon Ray", "Evil Containment Wave", "Wolf Fang Fist"],
+        correctAnswer: "Evil Containment Wave"
+
+    }
+]
+
